c3a25e86bc5f6cef93cf4f2b8eee36c03f362dbd
boraty14 <bratygame14@gmail.com>
2024-11-03 13:33:33 +0300

    added dpendencies

--- 2DGameEngine/src/Main.cpp
+++ 2DGameEngine/src/Main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 
-int main() {
+int main(int argc, char* argv[]) {
+	sol::state lua;
+	lua.open_libraries(sol::lib::base);
+
+	glm::vec2 velocity = glm::vec2(1.0f, 0.0f);
+
+	SDL_Init(SDL_INIT_EVERYTHING);
     std::cout << "Hello, world!" << std::endl;
+    std::cout << velocity.x << std::endl;
     return 0;
 }
